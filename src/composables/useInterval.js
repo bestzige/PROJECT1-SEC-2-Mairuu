b4ece33a9733cb00333889305da8b6c8dd3944f3
BestZige <bestzige@hotmail.com>
2024-01-26 02:32:51 +0700

    Change interval to intervalId for meaning

--- src/composables/useInterval.js
+++ src/composables/useInterval.js
@@ -2,20 +2,21 @@ import { onUnmounted, ref } from 'vue'
 
 export function useInterval(callback, ms = 1000) {
   const counter = ref(0)
-  let interval = null
+  let intervalId = null
 
   const start = () => {
-    if (interval !== null) return
-    interval = setInterval(() => {
-      counter.value++
-      callback()
-    }, ms)
+    if (intervalId === null) {
+      intervalId = setInterval(() => {
+        counter.value++
+        callback()
+      }, ms)
+    }
   }
 
   const stop = () => {
-    if (interval !== null) {
-      clearInterval(interval)
-      interval = null
+    if (intervalId !== null) {
+      clearInterval(intervalId)
+      intervalId = null
     }
   }
 
