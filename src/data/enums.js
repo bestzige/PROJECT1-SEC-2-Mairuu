10d4ab92924afbe25d65993b36243ca012a66735
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-01-26 13:45:45 +0700

    add data files

---
+++ src/data/enums.js
@@ -0,0 +1,15 @@
+/* Enum of game states */
+export const GameState = Object.freeze({
+  LOBBY: 'lobby',
+  STARTING: 'starting',
+  PLAYING: 'playing',
+  ENDING: 'ending',
+  RESULTS: 'results',
+})
+
+/* Enum of routes */
+export const RoutePath = Object.freeze({
+  HOME: 'Home Page',
+  GAME: 'Game Page',
+  MATCH_HISTORY: 'Match History Page',
+})
