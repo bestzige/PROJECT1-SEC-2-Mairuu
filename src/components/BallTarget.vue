79e007e7d56236ce925debb212bb251b8c004940
bestzige <bestzige@hotmail.com>
2024-01-27 22:19:34 +0700

    Create BallTarget.vue

---
+++ src/components/BallTarget.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { computed, defineProps } from 'vue'
+
+  const props = defineProps(['color', 'size'])
+
+  const containerClasses = [
+    'flex',
+    'justify-center',
+    'items-center',
+    'rounded-full',
+    'overflow-hidden',
+    'transition-all',
+    'duration-500',
+  ]
+
+  const containerStyle = computed(() => ({
+    width: props.size,
+    height: props.size,
+    backgroundColor: props.color,
+  }))
+</script>
+
+<template>
+  <div :style="containerStyle" :class="containerClasses" />
+</template>
