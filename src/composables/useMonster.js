6e4d143f4f5b48863a05dbb08fd14c330c697bd1
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-02-01 11:14:04 +0700

    update monsters.json and add useMonster.js
    Update monster.json by adding rangeCoin into each monster ,Add useMonster.js file into composables folder

---
+++ src/composables/useMonster.js
@@ -0,0 +1,38 @@
+import monsters from '@/data/monsters.json'
+import { reactive } from 'vue'
+
+export function useMonster() {
+  const getMonsterById = id => monsters.find(m => m.id === id)
+
+  const getDefaultMonster = () => {
+    const defaultMonster = monsters[0]
+    return { ...defaultMonster, health: defaultMonster.maxHealth }
+  }
+
+  const monster = reactive(getDefaultMonster())
+
+  const resetMonster = () => {
+    Object.assign(monster, getDefaultMonster())
+  }
+
+  const isMonsterDead = () => monster.health <= 0
+
+  const damageMonster = damage => {
+    monster.health -= damage
+
+    return isMonsterDead()
+  }
+
+  const nextMonster = () => {
+    const monsterIndex = monsters.findIndex(m => m.id === monster.id)
+    const nextMonster = monsters[monsterIndex + 1]
+
+    if (nextMonster) {
+      Object.assign(monster, { ...nextMonster, health: nextMonster.maxHealth })
+    }
+
+    return monsterIndex === monsters.length - 1 // Return true if this is the last monster
+  }
+
+  return { monster, getMonsterById, resetMonster, isMonsterDead, damageMonster, nextMonster }
+}
